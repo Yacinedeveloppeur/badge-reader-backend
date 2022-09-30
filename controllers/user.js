9bb94770bd766cda3756e230e18a83997de6bb72
Yacinedeveloppeur <yacine.developpeur@gmail.com>
2022-09-30 23:29:07 +0200

    .

--- controllers/user.js
+++ controllers/user.js
@@ -11,9 +11,9 @@ exports.signup = (req, res, next) => {
                 email: req.body.email,
                 password: hash
             });
-            user.save()
+            user.save({validatemodifyedOnly:true})
                 .then(() => { res.status(201).json({message: 'Utilisateur créé !'})})
-                .catch((error) => { res.status(500).json({ error })})
+                .catch((error) => { res.status(400).json({ error })})
         })
         .catch(error => {res.status(400).json({ error })})
 }
