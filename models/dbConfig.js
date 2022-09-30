1cf3a78b6cd79613cf6e6109eca60010fe17c7c6
Yacinedeveloppeur <yacine.developpeur@gmail.com>
2022-10-01 00:02:28 +0200

    .

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -4,7 +4,7 @@ const DB_URL = process.env.DB_URL;
 
 //connect to database
 mongoose
-  .connect(`${DB_URL}`, {
+  .connect(DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
