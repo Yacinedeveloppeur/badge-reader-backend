14b99c59ee54a53d9fee6d147b378f425c135b1f
Yacinedeveloppeur <yacine.developpeur@gmail.com>
2022-10-02 12:17:39 +0200

    .

--- controllers/badgeTime.js
+++ controllers/badgeTime.js
@@ -8,9 +8,8 @@ let momentdateTz = '';
 function generateFormatedDate() {
   moment.locale('fr');
   const timezone = 'Europe/Paris';
-  const date = moment();
+  const date = moment().format('YYYY-MM-DD HH:mm:ss');
   momentdateTz = momentTz.tz(date, timezone);
-  momentdateTz.format('YYYY-MM-DD HH:mm:ss');
   return momentdateTz;
 };
 
